b41f5929ac260f8b268152f8676deab9a10afd4a
AbdulRasol S. Al-Hilo <abdulrsol97@gmail.com>
2021-01-06 18:58:40 +0300

    fixing

--- static/js/actions.js
+++ static/js/actions.js
@@ -1,21 +1,20 @@
-// action btn
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".fixed-action-btn");
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: "top",
-  });
-});
-
 // select
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll("select");
   var instances = M.FormSelect.init(elems, {});
 });
 
+// back button
+document.getElementById("back-btn").onclick((e) => {
+  history.back();
+});
+
 // download file automaticlly
+/*
 setTimeout((e) => {
   document.getElementById("download-file").click();
   setTimeout((e) => {
     window.location.href = window.location.origin;
   }, 1500);
 }, 3000);
+*/
